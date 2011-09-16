e44b86d37f01cfb8d80506ace65cc9db4f4ab551
Betsy Haibel <betsy.haibel@gmail.com>
2011-09-15 21:32:53 -0400

    external interface passes data to backend properly

--- lib/stationer.rb
+++ lib/stationer.rb
@@ -1,5 +1,13 @@
 class Stationer
   def self.process(string)
-    
+    new(string).email
+  end
+  
+  def initialize(string)
+    @original = string
+  end
+
+  def email
+    true
   end
 end
--- spec/stationer_spec.rb
+++ spec/stationer_spec.rb
@@ -5,7 +5,21 @@ describe Stationer do
     it 'takes a string' do
       Stationer.process("test")
     end
-    it 'instantiates a new Stationer object'
-    it 'returns the string as processed by its Stationer instance'
+    
+    it 'instantiates a new Stationer object' do
+      Stationer.
+        should_receive(:new).
+        with("test").
+        and_return(double(Stationer).as_null_object)
+      Stationer.process("test")
+    end
+    
+    it 'returns the string as processed by its Stationer instance' do
+      instance = double(Stationer)
+      instance.should_receive(:email).and_return("result")
+      Stationer.stub!(:new).and_return(instance)
+      
+      Stationer.process("test").should == "result"
+    end
   end
 end
